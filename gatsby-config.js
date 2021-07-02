f6d958bca4c66c4ec746d3bce196282c9f86f5ad
Akshaj Jagarapu <askjag07@gmail.com>
2021-07-02 22:27:14 +0530

    Updated manifest configuration.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://divakar.fitness`,
+    siteUrl: 'https://divakar.fitness',
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-typescript`,
+      resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: true,
         allExtensions: true,
@@ -12,35 +12,35 @@ module.exports = {
         onlyRemoveTypeImports: true,
       },
     },
-    `gatsby-plugin-preact`,
+    'gatsby-plugin-preact',
     {
-      resolve: `gatsby-plugin-postcss`,
+      resolve: 'gatsby-plugin-postcss',
       options: {
-        postCssPlugins: [require(`postcss-preset-env`)({ stage: 3 })],
+        postCssPlugins: [require('postcss-preset-env')({ stage: 3 })],
       },
     },
-    `gatsby-plugin-sass`,
+    'gatsby-plugin-sass',
     {
-      resolve: `gatsby-plugin-purgecss`,
+      resolve: 'gatsby-plugin-purgecss',
       options: {
         printRejected: true,
         purgeCSSOptions: {
-          safelist: [`0%`, `100%`],
+          safelist: ['0%', '100%'],
         },
       },
     },
-    `gatsby-plugin-preload-fonts`,
+    'gatsby-plugin-preload-fonts',
     {
-      resolve: `gatsby-plugin-sharp`,
+      resolve: 'gatsby-plugin-sharp',
       options: {
         defaults: {
           placeholder: 'blurred',
         },
       },
     },
-    `gatsby-plugin-image`,
+    'gatsby-plugin-image',
     {
-      resolve: `gatsby-plugin-gatsby-cloud`,
+      resolve: 'gatsby-plugin-gatsby-cloud',
       options: {
         allPageHeaders: [
           'X-Frame-Options: SAMEORIGIN, always',
@@ -71,34 +71,37 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-loadable-components-ssr`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
+    'gatsby-plugin-loadable-components-ssr',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: `gatsby-plugin-robots-txt`,
+      resolve: 'gatsby-plugin-robots-txt',
       options: {
-        policy: [{ userAgent: `*`, allow: `/` }],
+        policy: [{ userAgent: '*', allow: '/' }],
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Divakar`,
-        short_name: `Divakar`,
-        lang: `en`,
-        start_url: `/`,
-        background_color: `#b86bff`,
-        theme_color: `#b86bff`,
-        display: `standalone`,
-        icon: `src/images/i.png`,
+        name: 'Divakar',
+        short_name: 'Divakar',
+        lang: 'en',
+        start_url: '/',
+        background_color: '#b86bff',
+        theme_color: '#b86bff',
+        display: 'standalone',
+        icon: './src/images/i.png',
         cache_busting_mode: 'none',
+        icon_options: {
+          purpose: 'any maskable',
+        },
       },
     },
     {
-      resolve: `gatsby-plugin-offline`,
+      resolve: 'gatsby-plugin-offline',
       options: {
         workboxConfig: {
-          globPatterns: [`./icons/icon*`],
+          globPatterns: ['./icons/icon*'],
         },
       },
     },
